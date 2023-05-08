23cf292fedfd90d5433aeb0815063f6751518534
rafaelogic <12.12afa3l@gmail.com>
2023-05-08 16:11:59 +0800

    update: tweaks and cleanup

---
+++ src/components/AppTaskButtonAddons.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="flex flex-row items-center px-1 mt-2 text-gray-300 hover:text-gray-700"
+  >
+    <slot name="buttonArea"></slot>
+  </div>
+</template>
+<script>
+export default {
+    name: 'AppNewTaskButton'
+}
+</script>
